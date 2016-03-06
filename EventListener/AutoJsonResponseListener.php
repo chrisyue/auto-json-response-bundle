e2dce26a13847fc4ba099181883c271bbed7636c
chrisyue <blizzchris@gmail.com>
2016-03-06 22:44:05 +0800

    ci

--- EventListener/AutoJsonResponseListener.php
+++ EventListener/AutoJsonResponseListener.php
@@ -1,16 +1,30 @@
 <?php
 
+/*
+ * This file is part of the ChrisyueAutoJsonResponseBundle package.
+ *
+ * (c) Chrisyue <http://chrisyue.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Chrisyue\Bundle\AutoJsonResponseBundle\EventListener;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
-use Symfony\Component\Serializer\NameConverter\CamelCaseToSnakeCaseNameConverter;
-use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
 use Symfony\Component\Serializer\Serializer;
 
 class AutoJsonResponseListener
 {
+    private $serializer;
+
+    public function __construct(Serializer $serializer = null)
+    {
+        $this->serializer = $serializer;
+    }
+
     public function onKernelView(GetResponseForControllerResultEvent $event)
     {
         if (!$event->isMasterRequest()) {
@@ -23,10 +37,13 @@ class AutoJsonResponseListener
         }
 
         $result = $event->getControllerResult();
-
         if (null === $result) {
             $event->setResponse(new JsonResponse(null, 204));
 
+            return;
+        }
+
+        if ($result instanceof Response) {
             return;
         }
 
@@ -37,14 +54,20 @@ class AutoJsonResponseListener
         }
 
         if (is_object($result)) {
-            $normalizer = new GetSetMethodNormalizer(null, new CamelCaseToSnakeCaseNameConverter());
-            $serializer = new Serializer([$normalizer]);
-            $result = $serializer->normalize($result);
+            $result = $this->getSerializer()->normalize($result);
         }
 
         $response->setData($result);
 
         $event->setResponse($response);
     }
-}
 
+    private function getSerializer()
+    {
+        if (null === $this->serializer) {
+            throw new \BadMethodCallException('You should enable `serializer` in `config.yml` to get this work');
+        }
+
+        return $this->serializer;
+    }
+}
